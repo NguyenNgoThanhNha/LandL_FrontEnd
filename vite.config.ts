bcaf97283934abab5fc49c5a2ece5212e92e9d70
hieutt03 <hieuttjob@gmail.com>
2024-11-01 00:41:30 +0700

    hide src

--- vite.config.ts
+++ vite.config.ts
@@ -9,15 +9,18 @@ export default defineConfig({
   },
   server: {
     watch: {
-      usePolling: true,
+      usePolling: true
     },
     host: true, // needed for the Docker Container port mapping to work
     strictPort: true,
-    port: 5173,
+    port: 5173
   },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
     }
+  },
+  build: {
+    sourcemap: false
   }
 })
